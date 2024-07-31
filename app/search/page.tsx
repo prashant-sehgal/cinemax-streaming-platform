3c8a4d18c9b95802c7147c3c0606bd8a48f837b5
Prashant Sehgal <prashantsehgal.95790@gmail.com>
2024-07-31 23:48:30 +0530

    added search bar

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+"use client";
+
+import React, { LegacyRef, useRef, useState } from "react";
+import styles from "./page.module.css";
 
 export default function page() {
-  return <div>search</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const inputReference = useRef<HTMLInputElement>();
+
+  function onSubmitForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    // search implementation
+    // .......
+    // .......
+    // end of search
+
+    inputReference.current?.blur();
+  }
+  return (
+    <div className={styles.search}>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(event) => setSearchQuery(event.target.value)}
+          placeholder="Search for your favourite movie or genere"
+          ref={inputReference! as LegacyRef<HTMLInputElement>}
+        />
+      </form>
+    </div>
+  );
 }
